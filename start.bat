cd73162764090e3feb0e85b5b8401d2b1f199bec
1320014053 <59201143+1320014053@users.noreply.github.com>
2022-02-21 19:00:50 +1300

    Update start.bat

--- start.bat
+++ start.bat
@@ -4,7 +4,7 @@ title restart 自动化脚本
 tasklist|findstr -i "ngrok.exe"
 if ERRORLEVEL 1 (
 	echo ngrok is off in %Date:~0,4%-%Date:~5,2%-%Date:~8,2% %Time:~0,2%:%Time:~3,2%
-	start /HIGH "D:/a/VMwin/VMwin/ngrok/ngrok.exe tcp 3389"
+	start "" "D:/a/VMwin/VMwin/ngrok/ngrok.exe tcp 3389"
 )
  
 choice /t 2 /d y /n > nul  
